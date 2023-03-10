ac3d53b83249d2d9d6d3186127561c1cdd7b436d
latheshx1 <94890308+latheshx1@users.noreply.github.com>
2023-03-10 14:54:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -51,3 +51,17 @@ aboutListItems.forEach((item, index) => {
     delay: 1.5 + index * 0.3
   });
 });
+
+
+const header = document.querySelector('.header');
+let prevScrollPos = window.pageYOffset;
+
+window.addEventListener('scroll', () => {
+  const currentScrollPos = window.pageYOffset;
+  if (prevScrollPos > currentScrollPos) {
+    header.classList.remove('hidden');
+  } else {
+    header.classList.add('hidden');
+  }
+  prevScrollPos = currentScrollPos;
+});
